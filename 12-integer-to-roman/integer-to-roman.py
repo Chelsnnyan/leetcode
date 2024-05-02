bc1fc0e0a60c068112067cc8d3e72a7d85edf94e
Chelsnnyan <133620852+Chelsnnyan@users.noreply.github.com>
2024-05-02 10:02:19 -0400

    Time: 42 ms (15.22%) | Memory: 11.5 MB (77.50%) - LeetSync

--- 12-integer-to-roman/integer-to-roman.py
+++ 12-integer-to-roman/integer-to-roman.py
@@ -4,55 +4,16 @@ class Solution(object):
         :type num: int
         :rtype: str
         """
-        val = ""
-        x=0
-        roman = {1:"I", 5:"V", 10:"X", 50:"L", 100:"C", 500:"D", 1000:"M"}
-        while num>0:
-            if num>=1000:
-                x = num
-                val+="M"
-                num-=1000
-            elif num<1000 and num>=900:
-                val+="CM"
-                num-=900
-            elif num<900 and num>=500:
-                val+="D"
-                num-=500
-            elif num<500 and num>=400:
-                val+="CD"
-                num-=400
-            elif num<400 and num>=100:
-                val+="C"
-                num-=100
-            elif num<100 and num>=90:
-                val+="XC"
-                num-=90
-            elif num<90 and num>=50:
-                val+="L"
-                num-=50
-            elif num<50 and num>=40:
-                val+="XL"
-                num-=40
-            elif num<40 and num>=10:
-                val+="X"
-                num-=10
-            elif num==9:
-                val+="IX"
-                num-=9
-
-            elif num<9 and num>=5:
-                val+="V"
-                num-=5
-            elif num==4:
-                val+="IV"
-                num-=4
-            else:
-                val+="I"
-                num-=1
-        return val
-            
-
-
-
+        sol = ""
         
+        list = [["I", 1],["IV", 4],["V", 5], ["IX", 9], ["X", 10], ["XL", 40],
+         ["L", 50], ["XC", 90], ["C", 100], ["CD", 400], ["D", 500],
+          ["CM", 900], ["M", 1000]]
 
+        for sym, val in reversed(list):
+            if num//val:
+                count = num//val
+                sol +=(sym*count)
+                num = num%val
+        return sol
+        
